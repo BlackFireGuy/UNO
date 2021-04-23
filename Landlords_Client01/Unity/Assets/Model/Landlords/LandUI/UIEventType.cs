ce61448d37d175092cef91a914ce6a8790092c5c
xyzsangxuan <54633318+xyzsangxuan@users.noreply.github.com>
2021-04-23 10:01:29 +0800

    登陆注册界面
    完成前端-登录注册界面和使用UI组件以及ReferenceCollector

---
+++ Landlords_Client01/Unity/Assets/Model/Landlords/LandUI/UIEventType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ETModel
+{
+    public static partial class LandUIType
+    {
+        public const string LandLogin = "LandLogin";
+    }
+
+    public static partial class UIEventType
+    {
+        public const string LandInitSceneStart = "LandInitSceneStart";
+    }
+
+    [Event(UIEventType.LandInitSceneStart)]
+    public class InitSceneStart_CreateLandLogin : AEvent
+    {
+        public override void Run()
+        {
+            //调用LandLoginFactory创建了登陆注册界面。
+            Game.Scene.GetComponent<UIComponent>().Create(LandUIType.LandLogin);
+        }
+    }
+}
